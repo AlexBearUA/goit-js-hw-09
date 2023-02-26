7b1ff69258fd07cf88dbef7f0350c39ca93edc97
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-02-26 14:51:29 +0100

    +100500

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -30,7 +30,7 @@ const options = {
 flatpickr('#datetime-picker', options);
 
 function onStartTimer() {
-  let timerId = setInterval(() => {
+  const timerId = setInterval(() => {
     refs.btnStart.disabled = true;
 
     const deltaTime = selectedDate - Date.now();
